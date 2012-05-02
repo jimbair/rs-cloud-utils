2358323cd5cb67b33140569ea8dc0c2c8bcca25c
James Bair <james.d.bair@gmail.com>
2012-05-02 02:44:25 +0000

    Renamed mysql dump script and moved mysql info into config.py

--- cloudsites-backup/bin/config.py
+++ cloudsites-backup/bin/config.py
@@ -5,3 +5,22 @@ apiKey = 'yourapigoeshere1234567'
 
 # You shouldn't have to change this.
 backupContainer = 'cloudsites'
+
+# For our DB backups
+# Just an example of how to de-duplicate entries
+db1 = '10.20.30.1'
+db2 = 'my-other-db.dc01.superhax.net'
+
+# Edit the following tuple of tuples to add the collection of Databases
+# you wish to backup. This must be done as we cannot dump all DBs with
+# MySQL dump, and you generally tie one DB per cloud site.
+#
+# Follows dbDump format:
+# User, pass, db, host
+databases = (
+            ( '123456_wordpress', 'l33tp4ssW0Rd', '123456_wpdb', db1 ),
+            ( '123456_phpbb', 'l33tp4ssW0Rd', '123456_phpbbdb', db1 ),
+            ( '123456_personal', 'l33tp4ssW0Rd', '123456_mydb', db2 ),
+            ( '123456_testing', 'l33tp4ssW0Rd', '123456_testdb', db2 ),
+            ( '123456_dev', 'l33tp4ssW0Rd', '123456_devdb', db2 )
+            )
